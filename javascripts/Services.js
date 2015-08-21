79e4222dfbd2d66fd43754e2b5742f104a0be7a8
DinamickaDevelopment <info@dinamicka.com>
2015-08-22 00:44:31 +0300

    123123

--- javascripts/Services.js
+++ javascripts/Services.js
@@ -45,4 +45,44 @@ jQuery(function ($) {
 
     //    window.pageXOffset = "0";
     //}
-}); 
+
+    var isMobile = {
+        Android: function () { return navigator.userAgent.match(/Android/i); },
+        BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+        iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+        Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+        Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+        any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+    };
+    //Проверка
+    //Height Configuration
+    if (!isMobile.any()) {
+    var winHeight = $(window).height();
+    var SectionHeight = winHeight - 90, containerHeight = SectionHeight * 0.85, containerFluidheight = SectionHeight * 0.15;
+
+    //Funnel section
+    $("#funnel").css("height", SectionHeight);
+
+    //Go-To-Marketing section;
+    $("#G2Market .container").css("height", containerHeight);
+    $("#G2Market .container-fluid .row:first-child").css("height", containerFluidheight);
+
+    $("#Sales .container").css("height", containerHeight);
+    $("#Sales .container-fluid .row:first-child").css("height", containerFluidheight);
+
+    $("#Contact_info .container").css("height", containerHeight);
+    $("#Contact_info .container-fluid .row:first-child").css("height", containerFluidheight);
+
+    $("#Awarness .container").css("height", containerHeight);
+    $("#Awarness .container-fluid .row:first-child").css("height", containerFluidheight);
+
+    $("#Marketing .container").css("height", containerHeight);
+    $("#Marketing .container-fluid .row:first-child").css("height", containerFluidheight);
+    } else {
+
+        $("#Stage").css('display', 'none');
+        $("#funnel").css("height", 'auto');
+        $('.forMobileBlock').css('display', 'block');
+    }
+
+});
