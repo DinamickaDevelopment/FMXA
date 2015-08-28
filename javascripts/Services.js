b7e784aafe823d528f5c45ee65cf4db6d6b74bc9
DinamickaDevelopment <info@dinamicka.com>
2015-08-28 18:57:32 +0300

    123123123

--- javascripts/Services.js
+++ javascripts/Services.js
@@ -8,11 +8,21 @@ jQuery(function ($) {
         location.reload();
     });
 
+    var isMobile = {
+        Android: function () { return navigator.userAgent.match(/Android/i); },
+        BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+        iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+        Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+        Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+        any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+    };
+
+    //<------------------- BTN-click logic ---------------------------------->
+
     for (var i = 0; i < document.getElementsByClassName("btn_container").length; i++) {
         document.getElementsByClassName("btn_container")[i].addEventListener("click", ClickFunction, true);
     }
 
-    //BTN-click logic
     function ClickFunction(e) {
         if (e.target.className == "bordered-btn" || e.target.className == "bordered-btn active-btn") {
             var id = "#" + e.target.dataset.idholder, section = "#" + e.target.dataset.section, selector;
@@ -32,37 +42,98 @@ jQuery(function ($) {
         }
   
     }
+    //<------------------- BTN-click logic -----------------END----------------->
+    // <------------------ Funnel Animation ---------------------------------->
+    if (!isMobile.any()) {//disable for mobile version site
+    window.addEventListener('scroll', Animation_revers, false);    var FunenAnimReady;
+    var funAnim = setTimeout(function () {
+        FunenAnimReady = true;
+    }, 4820);
 
-    ////BTN-click logic - end
-
-    //window.addEventListener("scroll", scroled, false);
-    //var flag = true;
-    //function scroled(e) {
-
-    //    var a = $('#Icons1_svg');
-
-    //    if (flag) {
-
-    //        $('#Icons1_svg').addClass('svg_2_anim');
-    
-    //    setTimeout(function () {
-    //        window.removeEventListener("scroll", scroled, false);
-    //    },2000);
+    DisableScroll(true);
+    } else {
+        document.getElementById("mobileBody").appendChild(document.getElementById("pseudoBody"));
+        document.getElementById("pseudoBody").style.display = "block";
+    }
 
 
-    //    }
+    function Animation_revers() {
+        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
 
-    //    window.pageXOffset = "0";
-    //}
+        if ($("#pseudoBody").css("display") == "block" && scrolled == 0) {
+            FunenAnimReady = false;
+            DisableScroll(true);
+            $("#pseudoBody").fadeOut("fast");
+            $('#SVG-container').css({ 'top': 'calc(50% - 202px)', 'left': 'calc(50% - 100px)', 'width': '200px', 'height': '85px' });
+            var reverstime,reverstime1 = setTimeout(function () {
+                $('#SVG-container').css("opacity", "0");
+                reverstime = setTimeout(function () {
+                    $('#Stage_Text_strips').css("clip", 'rect(0px, 894px,468px,0px)');
+                    $('#Stage_Icons_strips').css("left", '0');
+                    FunenAnimReady = true;
+                },100);
+            }, 700)
+        }
+    }
+    function DisableScroll(bool) {
+        if (bool) {
+            if (window.addEventListener) // older FF
+                window.addEventListener('DOMMouseScroll', preventDefault, false);
+            window.onwheel = preventDefault; // modern standard
+            window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+            window.ontouchmove  = preventDefault; // mobile
+            document.onkeydown  = preventDefaultForScrollKeys;
+        } else {
+            if (window.removeEventListener)
+                window.removeEventListener('DOMMouseScroll', preventDefault, false);
+            window.onmousewheel = document.onmousewheel = null;
+            window.onwheel = null;
+            window.ontouchmove = null;
+            document.onkeydown = null;
+        }
+        var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+        function preventDefault(e) {
+            e = e || window.event;
+            if (e.preventDefault)
+                e.preventDefault();
+            e.returnValue = false;
 
-    var isMobile = {
-        Android: function () { return navigator.userAgent.match(/Android/i); },
-        BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
-        iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
-        Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
-        Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
-        any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
-    };
+            //Animation
+            if (FunenAnimReady) {
+                $('#Stage_Text_strips').css("clip", 'rect(0px, 894px,468px,894px)');
+            $('#Stage_Icons_strips').css("left", 'calc(50% - 213px)');
+            var time1, time = setTimeout(function () {
+                $('#SVG-container').css({ 'top': '0', 'left': '0', 'width': '100%', 'height': '100%', 'opacity': '1' })
+                time1 = setTimeout(function () {
+                    $("#pseudoBody").fadeIn("fast");
+                    DisableScroll(false);
+                }, 1000);
+            }, 1900);
+            }//Animation - end
+        }
+        function preventDefaultForScrollKeys(e) {
+            if (keys[e.keyCode]) {
+                preventDefault(e);
+                return false;
+            }
+            //Animation
+            if (FunenAnimReady) {
+                $('#Stage_Text_strips').css("clip", 'rect(0px, 894px,468px,894px)');
+                $('#Stage_Icons_strips').css("left", 'calc(50% - 213px)');
+                var time1, time = setTimeout(function () {
+                    $('#SVG-container').css({ 'top': '0', 'left': '0', 'width': '100%', 'height': '100%', 'opacity': '1' })
+                    time1 = setTimeout(function () {
+                        $("#pseudoBody").fadeIn("fast");
+                        DisableScroll(false);
+                    }, 1000);
+                }, 1900);
+            }//Animation - end
+
+        }
+    }
+    // <------------------ Funnel Animation ---------END------------------------->
+    // <-----------------  Height Configuration ---------------------------------->
+
     //Проверка
     //Height Configuration
     if (!isMobile.any()) {
@@ -93,5 +164,5 @@ jQuery(function ($) {
         $("#funnel").css("height", 'auto');
         $('.forMobileBlock').css('display', 'block');
     }
-
+    // <------------------ mobile Check Function and Height Configuration --------END-------------------------->
 });
