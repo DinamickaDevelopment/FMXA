0bf97e2d203cf32c9634b0ae4ba2deaef0b32227
DinamickaDevelopment <info@dinamicka.com>
2015-09-14 23:48:12 +0300

    OurClients: Adaptive for mobile version

--- javascripts/our_clients.js
+++ javascripts/our_clients.js
@@ -1,38 +1,36 @@
-﻿$(document).ready(function () {
-    //document.getElementById('filter-btn-holder').addEventListener('click', FilterLogic, true);
-    //function FilterLogic(e) {
-    //    var targer = e.target;
-    //    filter = e.target.dataset.filter;
-    //    if (!targer.classList.contains(filter + '-sprt-active')) {
-    //        targer.classList.add(filter + '-sprt-active');
-    //        $('.company').each(function(i){
-    //            if (!this.classList.contains(filter)) {
-    //                this.style.display = "none";
-    //            }
-    //        });
-    //    } else {
-    //        targer.classList.remove(filter + '-sprt-active');
-    //        $('.company').each(function (i) {
-    //                this.style.display = "block";
-    //        });
-    //    }
-    //}
+﻿//Dinamicka Development
+//Documentation
+//
+//Mobile Check
+//Filter logick
+//Adaptive height
+
+
+$(document).ready(function () {
+    //Mobile Check
+    var isMobile = {
+        Android: function () { return navigator.userAgent.match(/Android/i); },
+        BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+        iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+        Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+        Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+        any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+    };
+
+    if (!isMobile.any()) {//mobile check
+    //---------------Filter logick----------------
     $('.filter-sprite').bind('click', filterLogick);
-
     var firstfilter = true;
-
     function filterLogick(e) {
         if (firstfilter) { AlldisplayNone(); }
         var filter = this.dataset.filter, CosmClass = filter + '-sprt-active';
-
-
         if (!e.target.classList.contains('active')) {
             e.target.classList.add('active');
             CosmClass = filter + '-sprt-active';
             e.target.classList.add(CosmClass);
             $('.company').each(function(i){
                 if (this.classList.contains(filter)) {
-                    this.style.display = "block";
+                    this.style.display = "inline-block";
                 }
             });
         } else {
@@ -40,25 +38,26 @@
             CosmClass = filter + '-sprt-active';
             e.target.classList.remove(CosmClass);
             $('.company').each(function (i) {
-                if (this.classList.contains(filter)) {
-                    this.style.display = "none";
-                }
+                if (this.classList.contains(filter)) {this.style.display = "none";}
             });
             if (!filterCheck()) {
-                $('.company').each(function (i) { this.style.display = "block"; });
+                $('.company').each(function (i) { this.style.display = "inline-block"; });
             }
         }
     }
-
     function AlldisplayNone() { $('.company').each(function (i) { this.style.display = "none"; }); firstfilter = false; }
-
     function filterCheck (e) {
         for (var i = 0; i < document.getElementsByClassName('filter-sprite').length; i++) {
-            if (document.getElementsByClassName('filter-sprite')[i].classList.contains('active'))
-                return true;
+            if (document.getElementsByClassName('filter-sprite')[i].classList.contains('active')) { return true; }
         }
         firstfilter = true;
         return false;
     }
-
+    //---------------Filter logick----end---------
+    //---------------Adaptive height--------------
+        var winHeight = $(window).height();
+        var SectionHeight = winHeight - 90;
+        $('#tech_area').css('height', SectionHeight);
+   //---------------Adaptive height---end--------
+    }
 });
